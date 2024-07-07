6b2eae6894f2da0dc84024352baff1f7c28f1fd0
tigertang <oncwnuKL8y-8gp5Vgb3owwYJ1ueg@git.weixin.qq.com>
2024-07-07 22:30:35 +0800

    feat: 水平导航 About

--- src/navigation/horizontal/index.ts
+++ src/navigation/horizontal/index.ts
@@ -5,7 +5,7 @@ export default [
     icon: { icon: 'ri-home-smile-line' },
   },
   {
-    title: 'Second page',
+    title: 'About',
     to: { name: 'second-page' },
     icon: { icon: 'ri-file-text-line' },
   },
