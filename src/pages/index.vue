6c5358a84c8f66662a38a4bf6edc3467d3ba9bbb
tigertang <oncwnuKL8y-8gp5Vgb3owwYJ1ueg@git.weixin.qq.com>
2024-07-21 09:27:11 +0800

    feat: 网站介绍文案

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,25 +1,42 @@
+<script setup lang="ts">
+console.log('pages/index.vue setup script')
+</script>
+
 <template>
   <div>
     <VCard
       class="mb-6"
-      title="Kick start your project 🚀"
+      title="Company Profile"
     >
-      <VCardText>All the best for your new project.</VCardText>
       <VCardText>
-        Please make sure to read our <a
-          href="https://JooTang.cn"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="text-decoration-none"
-        >
-          Template Documentation
-        </a> to understand where to go from here and how to use our template.
+        Shenzhen Belling Energy Efficiency Technology Co., Ltd. (referred to as Belling) was
+        established in 2015 and has consistently provided advanced quality assurance solutions for
+        our clients. Our business is always customer-centric, focusing on creating a first-class
+        customer service experience. Located in Longhua, Shenzhen, we are a pioneer in the domestic
+        third-party testing and certification technical service industry. Our company is established
+        in strict accordance with the requirements of ISO/IEC 17025:2017 and has obtained
+        recognition from the China National Accreditation Service for Conformity Assessment (CNAS)
+        and the China Metrology Accreditation (CMA), as well as the National Voluntary Laboratory
+        Accreditation Program (NVLAP) in the United States.
       </VCardText>
     </VCard>
-
-    <VCard title="Want to integrate JWT? 🔒">
-      <VCardText>We carefully crafted JWT flow so you can implement JWT with ease and with minimum efforts.</VCardText>
-      <VCardText>Please read our  JWT Documentation to get more out of JWT authentication.</VCardText>
+    <VCard
+      title="Efficient, Authoritative, and Impartial Third-Party Testing and Certification Technical
+        Service Institution"
+    >
+      <VCardText>
+        Currently, our company primarily focuses on the testing and international certification of
+        lighting, household appliances, audio-visual, and other electronic and electrical products.
+        We have been accredited by numerous international certification bodies such as UL, Intertek,
+        CSA, MET, TUV-RH, TUV-SUD, SGS, NEMKO, FCC, and IC, enabling us to provide professional and
+        authoritative one-stop testing and certification services for our clients.
+      </VCardText>
+      <VCardText>
+        Looking to the future, we will continue to innovate and enhance our expertise, offering
+        comprehensive services from testing to certification. We will persist in our efforts to
+        deliver high-quality service value, earn the trust of our clients, and help each client
+        achieve accelerated business growth and strengthened core competitiveness.
+      </VCardText>
     </VCard>
   </div>
 </template>
