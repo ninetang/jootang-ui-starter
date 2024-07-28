76df05ba4f33eb7d73f8590030a4191087a59231
tigertang <oncwnuKL8y-8gp5Vgb3owwYJ1ueg@git.weixin.qq.com>
2024-07-28 13:44:45 +0800

    feat: nav + 检测服务

--- src/navigation/horizontal/index.ts
+++ src/navigation/horizontal/index.ts
@@ -9,4 +9,9 @@ export default [
     to: { name: 'about' },
     icon: { icon: 'ri-file-text-line' },
   },
+  {
+    title: 'Testing Services',
+    to: { name: 'about' },
+    icon: { icon: 'ri-file-text-line' },
+  },
 ]
