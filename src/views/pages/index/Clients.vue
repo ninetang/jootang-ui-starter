ea72ad4da1aa6b02da16c6989d09d14cf238fabb
tigertang <oncwnuKL8y-8gp5Vgb3owwYJ1ueg@git.weixin.qq.com>
2024-07-28 13:44:31 +0800

    feat: index + clients 组件

---
+++ src/views/pages/index/Clients.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { register } from 'swiper/element/bundle'
+import swiper31 from '@images/clients/unilumin.png'
+import swiper32 from '@images/clients/hans.png'
+
+register()
+</script>
+
+<template>
+  <swiper-container
+    pagination-clickable="true"
+    slides-per-view="5"
+    space-between="50"
+    events-prefix="swiper-"
+    :breakpoints="{
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+    }"
+  >
+    <swiper-slide
+      v-for="swiperImg in [
+        swiper31,
+        swiper32,
+        swiper31,
+        swiper32,
+        swiper31,
+        swiper32,
+
+      ]"
+      :key="swiperImg"
+    >
+      <VImg :src="swiperImg" />
+    </swiper-slide>
+  </swiper-container>
+</template>
