c432a7f1e68fc1b601ae30f7610fbdc6c4ff2894
Almitor <2862710232@qq.com>
2024-08-01 10:24:25 +0800

    feat: 新增公司简介

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import Banner from '@/views/pages/index/Banner.vue'
 import Clients from '@/views/pages/index/Clients.vue'
+import pages5 from '@images/pages/5.jpg'
 </script>
 
 <template>
@@ -10,7 +11,48 @@ import Clients from '@/views/pages/index/Clients.vue'
     </VCol>
   </VRow>
 
-  <VRow>
+  <VRow class="introduction">
+    <VCol>
+      <VCard>
+        <div class="d-flex flex-column-reverse flex-md-row">
+          <div>
+            <VCardItem>
+              <VCardTitle>公司简介</VCardTitle>
+            </VCardItem>
+
+            <VCardText class="d-flex align-center flex-wrap text-body-1">
+              <span>Company Profile</span>
+            </VCardText>
+
+            <VCardText>
+              深圳市贝岭能效技术有限公司（简称Belling）成立于2015年，一直为客户提供更领先的质量保障整体
+              解决方案，我们的事业始终以“客户”为本，着重打造一流的客户服务体验。公司位于深圳市龙华，是
+              国内第三方检测认证技术服务机构的先行者，严格按照ISO/IEC 17025：2017的要求建立，并取得中
+              国合格评定国家认可委员会（CNAS）和中国计量认证(CMA)认可；以及美国国家实验室自愿认可程序
+              NVLAP的认可。
+            </VCardText>
+
+            <VCardActions>
+              <VBtn>
+                了解更多
+                <VIcon
+                  end
+                  icon="ri-arrow-right-line"
+                />
+              </VBtn>
+            </VCardActions>
+          </div>
+
+          <div class="ma-auto pa-5">
+            <VImg
+              :width="300"
+              :src="pages5"
+              class="rounded"
+            />
+          </div>
+        </div>
+      </VCard>
+    </VCol>
     <VCard
       class="mb-6"
       :title="$t('pages.index.Company Profile')"
@@ -46,7 +88,6 @@ import Clients from '@/views/pages/index/Clients.vue'
       </VCardText>
     </VCard>
   </VRow>
-
   <VRow>
     <VCard title="客户案例">
       <Clients />
@@ -58,10 +99,12 @@ import Clients from '@/views/pages/index/Clients.vue'
 @use "@core/scss/template/libs/swiper.scss";
 
 .v-row {
-  margin-top: -1.5rem;
-
+  margin-block: 3rem !important;
   .banner {
     padding: 0
   }
+}
+.introduction {
+  background-color: pink;
 }
 </style>
