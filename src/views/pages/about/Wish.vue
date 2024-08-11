c1996695e2521493ccb9d19f2f31e5c2eaf4a652
Almitor <2862710232@qq.com>
2024-08-11 13:32:44 +0800

    feat: 企业愿景

--- src/views/pages/about/Wish.vue
+++ src/views/pages/about/Wish.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import ctaDashborad from '@images/pages/app-search-header-bg-light.png'
 </script>
 
 <template>
@@ -8,29 +7,23 @@ import ctaDashborad from '@images/pages/app-search-header-bg-light.png'
       <div class="landing-cta position-relative bg-surface">
         <VContainer>
           <div class="d-flex align-center justify-sm-space-between flex-column flex-md-row gap-y-4 pt-9">
-            <div class="text-sm-start text-center py-8">
-              <div class="banner-text pb-1">
-                Ready to Get Started?
+            <div class="text-sm-start text-center py-8 w-50">
+              <div class="pb-1">
+                <h2>
+                  企业愿景
+                </h2>
               </div>
               <div class="text-body-1 font-weight-medium mb-8">
-                Start your project with a 14-day free trial
+                &emsp;&emsp;Belling成为检测认证行业最具创造力的服务商，真正意义上帮
+                助企业提升产品质量，为企业提供全方位的技术咨询和检测服务。
+                对接行业专家、整合社会资源（包括中小型企业资源，人力资源，
+                国家科研教育单位等）发挥渠道优势，打造强有力的执行团队，
+                为客户提供优质高效的服务。
               </div>
-              <VBtn
-                color="primary"
-                :append-icon="$vuetify.locale.isRtl ? 'ri-arrow-left-line' : 'ri-arrow-right-line'"
-                :to="{ name: '' }"
-              >
-                Get Started
-              </VBtn>
             </div>
-
-            <VImg
-              :src="ctaDashborad"
-              :max-width="$vuetify.display.mdAndUp ? 600 : ''"
-              max-height="250"
-              width="auto"
-              class="mb-n4"
-            />
+            <div class="banner-text">
+              诚信铸就品质<br>&emsp;&emsp;创新引领未来
+            </div>
           </div>
         </VContainer>
       </div>
