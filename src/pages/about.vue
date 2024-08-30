cccb3fb1dc898c5ba3f412159a09312cc5526d4e
Almitor <2862710232@qq.com>
2024-08-30 22:15:33 +0800

    feat: 服务项目 导航栏

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -5,22 +5,65 @@ import Team from '@/views/pages/about/Team.vue'
 import History from '@/views/pages/about/History.vue'
 import Wish from '@/views/pages/about/Wish.vue'
 import Culture from '@/views/pages/about/Culture.vue'
-import NavBar from "@/views/pages/about/NavBar.vue";
+import navImg from '@images/logo/nav.jpg'
+import NavBar from '@/views/pages/about/NavBar.vue'
+
+const sectionId = ref('')
+const profile = ref()
+const wish = ref()
+const history = ref()
+const certification = ref()
+const team = ref()
+const culture = ref()
+
+useIntersectionObserver(
+  [profile, wish, history, certification, team, culture],
+  ([{ isIntersecting, target }]) => {
+    if (isIntersecting)
+      sectionId.value = target.id
+  },
+  {
+    threshold: 0.25,
+  },
+)
 </script>
 
 <template>
+  <VRow>
+    <VImg :src="navImg" />
+  </VRow>
+  <!--  导航栏 -->
+  <NavBar :active-id="sectionId" />
   <!-- 公司简介 -->
-  <Profile />
+  <Profile
+    id="profile"
+    ref="profile"
+  />
   <!-- 企业愿景 -->
-  <Wish />
+  <Wish
+    id="wish"
+    ref="wish"
+  />
   <!-- 公司历史 -->
-  <History />
+  <History
+    id="history"
+    ref="history"
+  />
   <!-- 资质认证 -->
-  <Certification />
+  <Certification
+    id="certification"
+    ref="certification"
+  />
   <!-- 团队介绍 -->
-  <Team />
+  <Team
+    id="team"
+    ref="team"
+  />
   <!--  公司理念 -->
-  <Culture />
+  <Culture
+    id="culture"
+    ref="culture"
+  />
 </template>
 
 <style lang="scss">
--- src/views/pages/about/NavBar.vue
+++ src/views/pages/about/NavBar.vue
@@ -1,81 +1,59 @@
 <script setup lang="ts">
-import { useWindowScroll } from '@vueuse/core'
-import { useDisplay } from 'vuetify'
-
 const props = defineProps({
   activeId: String,
 })
 
-const display = useDisplay()
-
-const { y } = useWindowScroll()
-
-const sidebar = ref(false)
-
-watch(() => display, () => {
-  return display.mdAndUp ? sidebar.value = false : sidebar.value
-}, { deep: true })
-
-
+const navTabs = [
+  {
+    name: '公司简介',
+    value: 'profile',
+  },
+  {
+    name: '企业愿景',
+    value: 'wish',
+  },
+  {
+    name: '公司历史',
+    value: 'history',
+  },
+  {
+    name: '资质认证',
+    value: 'certification',
+  },
+  {
+    name: '团队介绍',
+    value: 'team',
+  },
+  {
+    name: '公司理念',
+    value: 'culture',
+  },
+]
 </script>
 
 <template>
-  <!-- 👉 Navbar for desktop devices  -->
-  <div class="front-page-navbar">
-    <VAppBar
-      :class="y > 20 ? 'front-page-navbar-box-shadow' : ''"
-      elevation="0"
-      class="rounded-b-lg"
+  <VRow class="d-flex text-h5 align-center gap-10 nav">
+    <RouterLink
+      v-for="item in navTabs"
+      :key="item.value"
+      class="nav-link font-weight-medium"
+      :to="{ name: 'about', hash: `#${item.value}` }"
+      :class="[props.activeId === item.value ? 'active-link' : '']"
     >
-      <!-- toggle icon for mobile device -->
-      <VAppBarNavIcon
-        :class="$vuetify.display.mdAndUp ? 'd-none' : 'd-inline-block'"
-        class="ms-0 me-1"
-        color="high-emphasis"
-        @click="() => sidebar = !sidebar"
-      />
-
-      <!-- Title and Landing page sections -->
-      <div class="d-flex align-center">
-        <!-- landing page sections -->
-        <div
-          :class="$vuetify.display.mdAndUp ? 'd-flex' : 'd-none'"
-          class="text-base align-center gap-x-2"
-        >
-          <RouterLink
-            v-for="(item, index) in ['Home', 'Features', 'Team', 'FAQ', 'Contact us']"
-            :key="index"
-            :to="{ name: 'about', hash: `#${item.toLowerCase().replace(' ', '-')}` }"
-            class="nav-link font-weight-medium"
-            :class="[props.activeId?.toLocaleLowerCase().replace('-', ' ') === item.toLocaleLowerCase() ? 'active-link' : '']"
-          >
-            {{ item }}
-          </RouterLink>
-        </div>
-      </div>
-    </VAppBar>
-  </div>
+      {{ item.name }}
+    </RouterLink>
+  </VRow>
 </template>
 
-<style lang="scss" scoped>
-.front-page-navbar-box-shadow{
-  /* stylelint-disable-next-line max-line-length */
-  box-shadow: 0 4px 8px -4px rgba(var(--v-shadow-key-umbra-color), 42%) !important;
+<style scoped lang="scss">
+.nav{
+  position: sticky;
+  top: 8rem;
+  background-color: #eeeeee;
+  margin-top: -1rem;
+  height: 4rem;
+  z-index: 9999;
 }
-
-.nav-menu{
-  display: flex;
-  gap: 3rem;
-}
-
-.nav-title{
-  color: rgba(var(--v-theme-on-surface), var(--v-high-emphasis-opacity)) !important;
-  font-size: 1.25rem;
-  font-weight: 600;
-  letter-spacing: 0.15px;
-  line-height: 1.5rem;
-}
-
 .nav-link{
   padding-inline: 0.625rem;
 
@@ -83,108 +61,7 @@ watch(() => display, () => {
     color: rgba(var(--v-theme-on-surface), var(--v-high-emphasis-opacity))
   }
 }
-
-@media (min-width: 1920px) {
-  .front-page-navbar {
-    .v-toolbar {
-      max-inline-size: calc(1440px - 32px);
-    }
-  }
-}
-
-@media (min-width: 1280px) and (max-width: 1919px) {
-  .front-page-navbar {
-    .v-toolbar {
-      max-inline-size: calc(1200px - 32px);
-    }
-  }
-}
-
-@media (min-width: 960px) and (max-width: 1279px) {
-  .front-page-navbar {
-    .v-toolbar {
-      max-inline-size: calc(900px - 32px);
-    }
-  }
-
-  .nav-menu{
-    gap: 2rem;
-  }
-}
-
-@media (min-width: 600px) and (max-width: 959px) {
-  .front-page-navbar {
-    .v-toolbar {
-      max-inline-size: calc(100% - 64px);
-    }
-  }
-}
-
-@media (max-width: 600px) {
-  .front-page-navbar {
-    .v-toolbar {
-      max-inline-size: calc(100% - 32px);
-    }
-  }
-}
-
-.nav-item-img {
-  border: 10px solid rgb(var(--v-theme-background));
-  border-radius: 10px;
-}
-
-.active-link {
+.active-link{
   color: rgb(var(--v-theme-primary)) !important;
-}
-
-.mega-menu-item {
-  &:not(:hover){
-    color: rgba(var(--v-theme-on-surface), var(--v-high-emphasis-opacity))
-  }
-}
-</style>
-
-<style lang="scss">
-@use "@layouts/styles/mixins" as layoutMixins;
-
-.mega-menu{
-  position: fixed !important;
-  inset-block-start: 4.1rem;
-  inset-inline-start: 50%;
-  transform: translateX(-50%);
-}
-
-.front-page-navbar {
-  .v-toolbar__content {
-    padding-inline: 2rem !important;
-  }
-
-  .v-toolbar {
-    inset-inline: 0 !important;
-    margin-inline: auto !important;
-  }
-}
-
-#navigation-drawer-close-btn {
-  position: absolute;
-  cursor: pointer;
-  inset-block-start: 0.5rem;
-  inset-inline-end: 1rem;
-}
-
-@media (max-width: 600px) {
-  .front-page-navbar {
-    .v-toolbar__content {
-      padding-inline: 0.75rem !important;
-    }
-  }
-}
-
-@media (min-width: 600px) and (max-width: 959px) {
-  .front-page-navbar {
-    .v-toolbar__content {
-      padding-inline: 1rem !important;
-    }
-  }
 }
 </style>
