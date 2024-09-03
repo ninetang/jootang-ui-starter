439a2303bdba41fbacef2ae58aba9e5ee515bb1a
Almitor <2862710232@qq.com>
2024-09-03 11:00:18 +0800

    feat: 关于我们 团队介绍 弹出成员信息

--- src/views/pages/about/Team.vue
+++ src/views/pages/about/Team.vue
@@ -10,70 +10,117 @@ const teamData = ref([
   { name: 'Chris Watkins', position: 'Marketing Manager', image: teamPerson3, backgroundColor: 'rgba(86, 202, 0, 0.16)', borderColor: 'rgba(86, 202, 0,0.38)', isHover: false },
   { name: 'Paul Miles', position: 'UI Designer', image: teamPerson4, backgroundColor: 'rgba(22, 177, 255, 0.16)', borderColor: 'rgba(22, 177, 255,0.38)', isHover: false },
 ])
+
+const isDialogVisible = ref(false)
+const member = ref()
+
+const showMessage = data => {
+  isDialogVisible.value = true
+  member.value = data
+  console.log(member.value.image)
+}
 </script>
 
 <template>
   <VRow>
-    <VCard class="w-100" >
-        <div class="our-team">
-          <h2 class="text-center">
-            团队介绍
-          </h2>
-          <VRow>
-            <VCol
-              v-for="(data, index) in teamData"
-              :key="index"
-              cols="12"
-              lg="3"
-              sm="6"
+    <VCard class="w-100">
+      <div class="our-team">
+        <h2 class="text-center">
+          团队介绍
+        </h2>
+        <VRow>
+          <VCol
+            v-for="(data, index) in teamData"
+            :key="index"
+            cols="12"
+            lg="3"
+            sm="6"
+          >
+            <VCard
+              flat
+              variant="outlined"
+              min-width="267"
+              class="position-relative overflow-visible mt-16"
+              :style="data.isHover ? { border: `1px solid ${data.borderColor}` } : {}"
+              @mouseenter="data.isHover = true"
+              @mouseleave="data.isHover = false"
+              @click="showMessage(data)"
             >
-              <VCard
-                flat
-                variant="outlined"
-                min-width="267"
-                class="position-relative overflow-visible mt-16"
-                :style="data.isHover ? { border: `1px solid ${data.borderColor}` } : {}"
-                @mouseenter="data.isHover = true"
-                @mouseleave="data.isHover = false"
-              >
-                <VImg
-                  :src="data.image"
-                  height="240px"
-                  class="team-image"
-                />
-                <div :style="{ maxHeight: '185px', minHeight: '185px', backgroundColor: data.backgroundColor }" />
-                <VCardText class="text-center">
-                  <div class="mb-3">
-                    <h5 class="text-h5">
-                      {{ data.name }}
-                    </h5>
-                    <div class="text-body-1 text-medium-emphasis">
-                      {{ data.position }}
-                    </div>
+              <VImg
+                :src="data.image"
+                height="240px"
+                class="team-image"
+              />
+              <div :style="{ maxHeight: '185px', minHeight: '185px', backgroundColor: data.backgroundColor }" />
+              <VCardText class="text-center">
+                <div class="mb-3">
+                  <h5 class="text-h5">
+                    {{ data.name }}
+                  </h5>
+                  <div class="text-body-1 text-medium-emphasis">
+                    {{ data.position }}
                   </div>
+                </div>
 
-                  <div class="d-flex gap-x-2 align-center justify-center">
-                    <template
-                      v-for="{ icon, color } in [
-                    { icon: 'ri-facebook-circle-line', color: 'rgba(59, 89, 152, 1)', link: 'https://www.facebook.com/' },
-                    { icon: 'ri-twitter-line', color: 'rgba(0, 172, 238, 1)', link: 'https://twitter.com/' },
-                    { icon: 'ri-linkedin-box-line', color: 'rgba(0, 119, 181, 1)', link: 'https://linkedin.com' },
-                  ]"
-                      :key="color"
-                    >
-                      <VIcon
-                        :icon="icon"
-                        size="22"
-                        :color="data.isHover ? color : ''"
-                        class="cursor-pointer"
-                      />
-                    </template>
-                  </div>
-                </VCardText>
+                <div class="d-flex gap-x-2 align-center justify-center">
+                  <template
+                    v-for="{ icon, color } in [
+                      { icon: 'ri-mail-line', color: 'rgba(0, 119, 181, 1)', link: 'https://linkedin.com' },
+                    ]"
+                    :key="color"
+                  >
+                    <VIcon
+                      :icon="icon"
+                      size="22"
+                      :color="data.isHover ? color : ''"
+                      class="cursor-pointer"
+                    />
+                  </template>
+                </div>
+              </VCardText>
+            </VCard>
+            <VDialog
+              v-model="isDialogVisible"
+              width="500"
+            >
+              <VCard title="成员信息">
+                <DialogCloseBtn
+                  variant="text"
+                  size="default"
+                  @click="isDialogVisible = false"
+                />
+                <VRow>
+                  <VCol
+                    cols="12"
+                    md="5"
+                    class="pa-5"
+                  >
+                    <VImg
+                      :src="member.image"
+                      width="200"
+                      class="ma-auto"
+                    />
+                  </VCol>
+                  <VCol
+                    cols="12"
+                    md="7"
+                  >
+                    <VCardText class="text-h5">
+                      {{ member.name }}
+                    </VCardText>
+                    <VCardText class="text-h5">
+                      {{ member.position }}
+                    </VCardText>
+                    <VCardText class="text-body-1">
+                      content
+                    </VCardText>
+                  </VCol>
+                </VRow>
               </VCard>
-            </VCol>
-          </VRow>
-        </div>
+            </VDialog>
+          </VCol>
+        </VRow>
+      </div>
     </VCard>
   </VRow>
 </template>
