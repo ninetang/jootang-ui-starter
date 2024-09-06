6ff2250ed283a1c2eb870e4f013747e0ee7d9965
Almitor <2862710232@qq.com>
2024-09-06 15:19:45 +0800

    feat: 证书查询 响应式布局

--- src/pages/info-query/index.vue
+++ src/pages/info-query/index.vue
@@ -17,71 +17,64 @@ function navigateTo() {
   <TopImg />
   <VRow>
     <VCard
-      class="w-100"
+      class="w-100 pa-3"
       title="证书查询"
     >
       <VRow class="d-flex justify-center">
         <VCol
           cols="12"
           md="4"
+          sm="8"
+          xs="10"
+          class="d-flex flex-column align-center justify-space-around"
         >
-          <VForm @submit.prevent="() => {}">
-            <VRow>
-              <VCol cols="12">
-                <VTextField
-                  v-model="code"
-                  label="证书编号"
-                  placeholder="输入完整编号"
-                  :rules="[requiredValidator]"
-                />
-              </VCol>
-
-              <VCol
-                cols="12"
-                class="d-flex flex-wrap justify-center gap-4"
-              >
-                <VDialog
-                  v-model="isDialogVisible"
-                  width="500"
-                >
-                  <VCard title="查询失败">
-                    <DialogCloseBtn
-                      variant="text"
-                      size="defalut"
-                      @click="isDialogVisible = false"
-                    />
-                    <VCardText>
-                      证书号不能为空
-                    </VCardText>
-                    <VCardActions>
-                      <VSpacer />
-                      <VBtn @click="isDialogVisible = false">
-                        确定
-                      </VBtn>
-                    </VCardActions>
-                  </VCard>
-                </VDialog>
-                <VBtn
-                  size="large"
-                  class="w-100"
-                  type="submit"
-                  @click="navigateTo"
-                >
-                  查询
+          <div class="w-100">
+            <VTextField
+              v-model="code"
+              label="证书编号"
+              placeholder="输入完整编号"
+              :rules="[requiredValidator]"
+            />
+          </div>
+          <VDialog
+            v-model="isDialogVisible"
+            width="500"
+          >
+            <VCard title="查询失败">
+              <DialogCloseBtn
+                variant="text"
+                size="defalut"
+                @click="isDialogVisible = false"
+              />
+              <VCardText>
+                证书号不能为空
+              </VCardText>
+              <VCardActions>
+                <VSpacer />
+                <VBtn @click="isDialogVisible = false">
+                  确定
                 </VBtn>
+              </VCardActions>
+            </VCard>
+          </VDialog>
+          <VBtn
+            size="large"
+            class="w-100 ma-5"
+            type="submit"
+            @click="navigateTo"
+          >
+            查询
+          </VBtn>
 
-                <VBtn
-                  type="reset"
-                  color="secondary"
-                  variant="tonal"
-                  size="large"
-                  class="w-100"
-                >
-                  重置
-                </VBtn>
-              </VCol>
-            </VRow>
-          </VForm>
+          <VBtn
+            type="reset"
+            color="secondary"
+            variant="tonal"
+            size="large"
+            class="w-100"
+          >
+            重置
+          </VBtn>
         </VCol>
       </VRow>
     </VCard>
