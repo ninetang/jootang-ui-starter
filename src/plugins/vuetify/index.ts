f78e87d4841ff60353582986e110a194612ef165
Almitor <2862710232@qq.com>
2024-10-20 15:24:10 +0800

    feat: 语言包

--- src/plugins/vuetify/index.ts
+++ src/plugins/vuetify/index.ts
@@ -2,6 +2,7 @@ import { deepMerge } from '@antfu/utils'
 import type { App } from 'vue'
 import { createVuetify } from 'vuetify'
 import { VBtn } from 'vuetify/components/VBtn'
+import { zhHans } from 'vuetify/lib/locale/index.mjs'
 import defaults from './defaults'
 import { icons } from './icons'
 import { staticPrimaryColor, staticPrimaryDarkenColor, themes } from './theme'
@@ -39,7 +40,10 @@ export default function (app: App) {
     defaults,
     icons,
     theme: optionTheme,
-
+    locale: {
+      locale: 'zhHans',
+      messages: { zhHans },
+    },
   })
 
   app.use(vuetify)
